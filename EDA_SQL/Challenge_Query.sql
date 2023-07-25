325c2a33065c60d14433da2e03479b60fb44d373
Ega Fernanda Putra <dragoon.fernanda12@gmail.com>
2023-07-25 19:21:46 +0700

    Update EDA with SQL and the result

--- EDA_SQL/Challenge_Query.sql
+++ EDA_SQL/Challenge_Query.sql
@@ -1,13 +1,19 @@
 
 -- Average Age based on Marital Status
-SELECT "Marital Status" , AVG(age) AS average_age
+SELECT "Marital Status" , ROUND(AVG(age)) AS average_age
 FROM fpkalbe.customer c
 where "Marital Status" IS NOT NULL 
 AND "Marital Status" <> ''
 GROUP BY "Marital Status" ;
 
 -- Average Age based on Gender
-select gender, avg(age) as average_age
+select 
+	case 
+		when gender = 0 then 'Female'
+		when gender = 1 then 'Male'
+		else 'Unknown'
+	end as gender,
+	ROUND(avg(age)) as average_age
 from fpkalbe.customer c 
 where gender is not null 
 group by gender 
@@ -18,7 +24,7 @@ from fpkalbe.store s
 join fpkalbe."transaction" t on s.storeid = t.storeid 
 group by s.storename 
 order by total_quantity desc 
-limit 5 ;
+limit 1 ;
 
 -- Product Name with Highest Selling
 select p."Product Name" , sum(totalamount) as total_amount, sum(qty) as total_quantity
@@ -26,3 +32,4 @@ from fpkalbe.product p
 join fpkalbe."transaction" t on p.productid = t.productid 
 group by p."Product Name" 
 order by total_amount desc 
+limit 1 ;
